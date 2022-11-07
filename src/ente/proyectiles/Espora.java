990f1a21ddd5ae9e24c0c691630e37e30fab7fe5
SaezBenja <saezb3nja@gmail.com>
2022-11-07 14:26:14 -0300

    Clases espora y guizante

--- src/ente/proyectiles/Espora.java
+++ src/ente/proyectiles/Espora.java
@@ -1,1 +1,64 @@
+package ente.proyectiles;
 
+import java.awt.Point;
+import java.util.Properties;
+
+import ente.grafico.EnteGrafico;
+import ente.zombi.visitor.Visitor;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Espora.
+ */
+
+public class Espora extends Proyectil {
+	
+	/** The distancia. */
+	private int distancia;
+	
+	/**
+	 * Instantiates a new espora.
+	 *
+	 * @param punto the punto
+	 * @param grafico the grafico
+	 * @param velocidad the velocidad
+	 * @param distancia the distancia
+	 * @param danio the danio
+	 */
+	public Espora(Point punto,EnteGrafico grafico,int velocidad, int distancia, int danio, Properties p ) {
+		//Generear archivo de configureacion.
+		this.config=p;
+		this.setLocation(punto);
+		this.width= Integer.parseInt(config.getProperty("ancho_espora"));
+		this.height=Integer.parseInt(config.getProperty("alto_espora"));
+		this.setLocation(punto);
+		this.grafico=grafico;
+		this.danio=danio;
+		this.velocidad=velocidad;
+		this.distancia=distancia;
+		this.grafico=new EnteGrafico(punto, this.width, this.height, config.getProperty("skin_espora"));
+		this.danio=Integer.parseInt(config.getProperty("danio_espora"));
+		this.velocidad=Integer.parseInt(config.getProperty("velovidad_espora"));
+		this.distancia=Integer.parseInt(config.getProperty("distancia_espora"));
+
+	}
+
+	/**
+	 * Accept.
+	 *
+	 * @param v the v
+	 */
+	public void accept(Visitor v) {
+		v.visitProyectil(this);
+	}
+	
+	/**
+	 * Gets the distancia.
+	 *
+	 * @return Distancia a la que llega la espora.
+	 */
+	public int getDistancia() {
+		return this.distancia;}	
+	{
+	}
+}
