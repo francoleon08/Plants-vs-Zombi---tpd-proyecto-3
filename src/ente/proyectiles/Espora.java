0109c5e7cf0989a7940ccf5d417a334b39a87f00
Franco Leon <francoleon08@hotmail.com>
2022-11-07 12:45:57 -0300

    Merge branch 'main' of https://github.com/tonyy7/tdp-proyecto-3 into main

--- src/ente/proyectiles/Espora.java
+++ src/ente/proyectiles/Espora.java
@@ -1,5 +1,13 @@
 package ente.proyectiles;
 
+import ente.zombi.visitor.Visitor;
+
 public class Espora extends Proyectil {
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/ente/proyectiles/Guizante.java
+++ src/ente/proyectiles/Guizante.java
@@ -1,6 +1,7 @@
 package ente.proyectiles;
 
 import ente.grafico.EnteGrafico;
+import ente.zombi.visitor.Visitor;
 
 public class Guizante extends Proyectil {
 	public Guizante(int x, int y, int width, int height, EnteGrafico grafico) {
@@ -9,6 +10,12 @@ public class Guizante extends Proyectil {
 		this.width=width;
 		this.height=height;
 		this.grafico=grafico;
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
