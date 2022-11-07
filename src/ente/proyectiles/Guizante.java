2a3dc4aaf4984686148e163680896efb6756bc59
SaezBenja <saezb3nja@gmail.com>
2022-11-07 10:55:20 -0300

    Clase Guizante

--- src/ente/proyectiles/Guizante.java
+++ src/ente/proyectiles/Guizante.java
@@ -1,7 +1,14 @@
 package ente.proyectiles;
 
+import ente.grafico.EnteGrafico;
+
 public class Guizante extends Proyectil {
-	public Guizante() {
-			
+	public Guizante(int x, int y, int width, int height, EnteGrafico grafico) {
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.grafico=grafico;
 	}
+	
 }
