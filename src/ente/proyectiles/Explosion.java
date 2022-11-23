f932f2c574fab9b376d4c7dd85067df33718aaf9
Agustín <oyaagustin@gmail.com>
2022-11-23 14:33:12 -0300

    Explosiones B)

--- src/ente/proyectiles/Explosion.java
+++ src/ente/proyectiles/Explosion.java
@@ -14,12 +14,13 @@ public class Explosion extends Proyectil{
 	public Explosion(Point position, Properties p) {
 		this.choqueZombie = false;
 		this.config = p;
-		this.setLocation(0, (int) position.getY());
+		Point px = new Point(0, (int) position.getY());
+		this.setLocation(px);
 		this.width= Integer.parseInt(config.getProperty("ancho_explosion"));
 		this.height=Integer.parseInt(config.getProperty("alto_explosion"));
 		this.danio=Integer.parseInt(config.getProperty("danio_explosion"));
 		this.velocidad=Integer.parseInt(config.getProperty("velocidad_explosion"));
-		this.grafico=new EnteGrafico(position, this.width, this.height, config.getProperty("skin_explosion"));
+		this.grafico=new EnteGrafico(px, this.width, this.height, config.getProperty("skin_explosion"));
 		this.duracion = 100*Integer.parseInt(config.getProperty("duracion_explosion"));
 	}
 	
