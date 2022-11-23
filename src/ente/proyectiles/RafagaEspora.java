9b25fa3c670039f804e5cfbb969ab5ab136b4356
Agustín <oyaagustin@gmail.com>
2022-11-23 14:52:17 -0300

    Ajustes menores

--- src/ente/proyectiles/RafagaEspora.java
+++ src/ente/proyectiles/RafagaEspora.java
@@ -10,15 +10,16 @@ public class RafagaEspora extends Proyectil{
 	private int duracion;
 	
 	public RafagaEspora(Point position, Properties p) {
-		this.setLocation(position);
 		this.config = p;		
 		this.choqueZombie = false;
+		Point px = new Point((int) position.getX()+30, (int) position.getY());
+		this.setLocation(px);
 		this.width = Integer.parseInt(config.getProperty("ancho_rafaga_espora"));
 		this.height = Integer.parseInt(config.getProperty("alto_rafaga_espora"));
 		this.danio = Integer.parseInt(config.getProperty("danio_rafaga_espora"));
 		this.velocidad = Integer.parseInt(config.getProperty("velocidad_rafaga_espora"));
 		this.duracion = 100*Integer.parseInt(config.getProperty("duracion_rafaga_espora"));
-		this.grafico = new EnteGrafico(this.getLocation(), this.width, this.height, config.getProperty("skin_rafaga_espora"));
+		this.grafico = new EnteGrafico(px, this.width, this.height, config.getProperty("skin_rafaga_espora"));
 	}
 
 	@Override
